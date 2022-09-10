c5d257281b2699014023bb80a4cb4435e4101932
GwangYoung <gospotb1607@gmail.com>
2022-09-10 13:16:51 +0900

    java-spring frist

---
+++ nine1ll-spring/src/main/java/Nine1ll/nine1llspring/Nine1llSpringApplication.java
@@ -0,0 +1,13 @@
+package Nine1ll.nine1llspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nine1llSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nine1llSpringApplication.class, args);
+	}
+
+}
---
+++ nine1ll-spring/src/test/java/Nine1ll/nine1llspring/Nine1llSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package Nine1ll.nine1llspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nine1llSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
